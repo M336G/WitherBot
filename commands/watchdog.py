e45e759faff517d1ba827c4d472f52244395b276
M336 <75208577+M336G@users.noreply.github.com>
2024-04-21 01:27:49 +0200

    remove waiting time

--- commands/watchdog.py
+++ commands/watchdog.py
@@ -18,13 +18,11 @@ def commandFunction(tree, client):
         ).json()
 
         if res["success"] == True:
-            embed = Embed(title=" ",description="**Getting data from the API, please wait...**",colour=9807270)
-            await interaction.response.send_message(" ",embed=embed, ephemeral=True)
             embed = Embed(title=f"**Watchdog Stats**",description=f'> **Total players banned by Watchdog in the last minute:** ``{res["watchdog_lastMinute"]}``\n> **Total players banned by Watchdog today:** ``{res["watchdog_rollingDaily"]}``\n> **Total players banned by Watchdog:** ``{res["watchdog_total"]}``\n\n> **Total players banned by Hypixel Staff Members today:** ``{res["staff_rollingDaily"]}``\n> **Total players banned by Hypixel Staff Members:** ``{res["staff_total"]}``')
             embed.set_thumbnail(url=f"https://yt3.googleusercontent.com/ytc/AIf8zZSv_62EYjr0w3lqr0PydI8vBsdscbUlMCYyWghH6g=s176-c-k-c0x00ffffff-no-rj")
             embed.set_footer(text=f"{client.user.name}", icon_url=f"{client.user.avatar}")
             embed.timestamp = datetime.now()
-            await interaction.channel.send(" ",embed=embed)
+            await interaction.response.send_message(" ", embed=embed)
 
             log(f"(SUCCESS) {interaction.user} used /watchdog")
                 
