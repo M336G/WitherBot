aefbd7d833bfdac1b3c17dc52187f19525b84492
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2024-06-22 23:20:50 +0300

    Create reminder.py

---
+++ commands/reminder.py
@@ -0,0 +1,15 @@
+@bot.slash_command(name='reminder', description='test')
+async def reminderCommand(interaction: Interaction, time, text):
+    time = int(time)
+    if time == 0:
+        embed = Embed(title=" ",description=f":x: **Please, choose any time what bigger than 0**", colour=15548997)
+        await interaction.response.send_message(" ",embed=embed, ephemeral=True)  
+    if not text:
+        embed = Embed(title=" ",description=f":x: **Please, write text**", colour=15548997)
+        await interaction.response.send_message(" ",embed=embed, ephemeral=True) 
+    else:
+        embed = Embed(title=" ",description=f":x: **The text will be sent in: {time} seconds **", colour=15548997)
+        await interaction.response.send_message(" ",embed=embed, ephemeral=True)
+        await asyncio.sleep(time)
+        embed = Embed(title="Reminder",description=f":x: {text}", colour=15548997)
+        await interaction.user.send(" ", embed=embed)
